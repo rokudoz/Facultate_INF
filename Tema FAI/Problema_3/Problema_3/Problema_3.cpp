2bbcd063ea4b61f108cb7646f3a05fcf34af6ed6
Robert Paraschiv <paraschivlongin@gmail.com>
2019-12-06 09:56:28 +0200

    changed Probl3

--- Tema FAI/Problema_3/Problema_3/Problema_3.cpp
+++ Tema FAI/Problema_3/Problema_3/Problema_3.cpp
@@ -2,26 +2,32 @@
 
 using namespace std;
 
+int cmmdc(int a, int b);
+int cmmmc(int a, int b);
+
+int main()
+{
+	int m, n;
+
+	cout << "m= ";
+	cin >> m;
+	cout << "n= ";
+	cin >> n;
+
+	cout << "CMMDC al " << m << " si " << n << " este: " << cmmdc(m, n);
+	cout << endl;
+	cout << "CMMMC al " << m << " si " << n << " este: " << cmmmc(m, n);
+	return 0;
+}
+
 int cmmdc(int a, int b)
 {
 	if (b == 0)
 		return a;
 	return cmmdc(b, a % b);
-
 }
 
 int cmmmc(int a, int b)
 {
 	return (a * b) / cmmdc(a, b);
-}
-
-// Driver program to test above function 
-int main()
-{
-	int m, n;
-	cin >> m >> n;
-	cout << "CMMDC al " << m << " si " << n << " este: " << cmmdc(m, n);
-	cout << endl;
-	cout << "CMMMC al " << m << " si " << n << " este: " << cmmmc(m, n);
-	return 0;
 }
